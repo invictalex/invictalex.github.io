795f3895961529155b00fdc3f8c399cc93e4df6e
Alex Christodoulou <alexchristodoulou@ymail.com>
2022-07-05 15:04:26 +0100

    Basic setup & complete welcome page

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 var sections = document.querySelectorAll(".section");
 var nextBtn = document.querySelector(".next");
 var prevBtn = document.querySelector(".prev");
+var navOptions = document.querySelectorAll(".option");
+var welcOption = document.querySelector(".welc-opt")
+var skillOption = document.querySelector(".skill-opt")
+var projOption = document.querySelector(".proj-opt")
+var expOption = document.querySelector(".exp-opt")
+var slider = document.querySelector(".slide-pointer")
+
+
 var currentSection = 0;
 
 
@@ -8,8 +16,19 @@ var currentSection = 0;
 toFirstSection();
 
 nextBtn.onclick = () => slideLeft();
+document.onkeydown = (e) => 
+{ if (e["key"] == "ArrowRight") 
+    slideLeft();
+ else if (e["key"] == "ArrowLeft")
+    slideRight();
+};
 
 prevBtn.onclick = () => slideRight();
+
+welcOption.onclick = () => slideTo(0);
+skillOption.onclick = () => slideTo(1);
+projOption.onclick = () => slideTo(2);
+expOption.onclick = () => slideTo(3);
 
 
 //                                              FUNCTION DEFINITIONS
@@ -22,7 +41,9 @@ function toFirstSection()
     sections.forEach((section, i) =>
     {
         section.style.transform = `translateX(${i*100}%)`;
-    }) 
+    })
+    
+    moveSlider(currentSection);
 }
 
 function toLastSection()
@@ -34,6 +55,7 @@ function toLastSection()
         section.style.transform = `translateX(${(i-3)*100}%)`;
     }) 
     
+    moveSlider(currentSection);
 }
 
 function slideLeft()
@@ -52,6 +74,8 @@ function slideLeft()
     {
         toFirstSection();
     }
+
+    moveSlider(currentSection);
 }
 
 function slideRight()
@@ -69,4 +93,47 @@ function slideRight()
     {
         toLastSection();
     } 
+    moveSlider(currentSection);
+
+}
+
+function slideTo(secNum)
+{
+    currentSection = secNum;
+
+    sections.forEach((section, i) =>
+    {
+        section.style.transform = `translateX(${(i - currentSection)*100}%)`;
+    })
+    moveSlider(currentSection);
+
+}
+
+
+function moveSlider(val)
+{
+    switch(val)
+    {
+        case 0: 
+        {
+            slider.style.transform = `translateX(0%)`;
+            slider.style.width = "112.7px";
+            break;
+        } case 1:
+        {
+            slider.style.transform = `translateX(142.7px)`;
+            slider.style.width = "60.69px";
+            break;
+        }case 2:
+        {
+            slider.style.transform = `translateX(233.39px)`;
+            slider.style.width = "103.63px";
+            break;
+        }case 3:
+        {
+            slider.style.transform = `translateX(367.02px)`;
+            slider.style.width = "141.75px";
+            break;
+        }
+    }
 }
